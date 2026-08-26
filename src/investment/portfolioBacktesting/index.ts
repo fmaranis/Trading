49a8f33bc8df39cdb384d3285ad7a69921415e1b
fmaranis <fmaranis@gmail.com>
2026-08-27 00:09:49 +0200

    feat(portfolio): export rolling allocation engine

--- src/investment/portfolioBacktesting/index.ts
+++ src/investment/portfolioBacktesting/index.ts
@@ -2,4 +2,5 @@ export * from './types';
 export * from './multiAssetDataAligner';
 export * from './rebalanceEngine';
 export * from './portfolioMetrics';
+export * from './rollingAllocationEngine';
 export * from './portfolioBacktestEngine';
