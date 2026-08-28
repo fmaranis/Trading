0c23c3ccbc6d60710aad89c4195aabca78ba03dc
fmaranis <fmaranis@gmail.com>
2026-08-28 23:48:12 +0200

    Export broker availability evidence

--- src/investment/decision/index.ts
+++ src/investment/decision/index.ts
@@ -4,6 +4,7 @@ export * from './decisionHistoryService';
 export * from './decisionBacktestEngine';
 export * from './assetUniverse';
 export * from './assetUniverseScanner';
+export * from './brokerAvailability';
 export * from './causalUniverseBacktestEngine';
 export * from './brokerExecution';
 export * from './brokerBacktestFeasibility';
