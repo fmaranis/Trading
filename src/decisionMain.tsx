4cfb853959008c36ffb8e363a91ec7a5375f715c
fmaranis <fmaranis@gmail.com>
2026-08-28 22:47:31 +0200

    Remove duplicate provider status from primary decision page

--- src/decisionMain.tsx
+++ src/decisionMain.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import { InteractiveInvestmentDecisionCenter } from './components/InteractiveInvestmentDecisionCenter';
-import { MarketProviderStatus } from './components/MarketProviderStatus';
 import './index.css';
 
 function DecisionApp() {
@@ -19,7 +18,6 @@ function DecisionApp() {
         </div>
       </header>
       <main className="mx-auto w-full max-w-7xl px-3 py-5 sm:px-6 lg:px-8">
-        <MarketProviderStatus />
         <InteractiveInvestmentDecisionCenter />
       </main>
       <footer className="mx-auto max-w-7xl px-4 pb-8 text-center text-[11px] text-slate-600">
