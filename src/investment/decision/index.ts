749c421feba25b8e9123b357ae1a894f9b1cb0bd
fmaranis <fmaranis@gmail.com>
2026-08-29 10:59:12 +0200

    Export dynamic historical replay engine

--- src/investment/decision/index.ts
+++ src/investment/decision/index.ts
@@ -9,6 +9,7 @@ export * from './cashBenchmark';
 export * from './remuneratedCash';
 export * from './causalUniverseBacktestEngine';
 export * from './historicalDecisionReplay';
+export * from './dynamicHistoricalReplay';
 export * from './strategyConsensusEngine';
 export * from './brokerExecution';
 export * from './brokerBacktestFeasibility';
