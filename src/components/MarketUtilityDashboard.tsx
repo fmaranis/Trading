59f1146210927cc1130e4c222eb5ade05d04d1b1
fmaranis <fmaranis@gmail.com>
2026-09-03 09:33:51 +0200

    feat: show cash growth in live portfolio flow

--- src/components/MarketUtilityDashboard.tsx
+++ src/components/MarketUtilityDashboard.tsx
@@ -10,6 +10,7 @@ import { CurrentOpportunityAlertsPanel } from './CurrentOpportunityAlertsPanel';
 import { UserPortfolioPanel } from './UserPortfolioPanel';
 import { PortfolioExecutionPlanPanel } from './PortfolioExecutionPlanPanel';
 import { RealPurchaseRegistrationPanel } from './RealPurchaseRegistrationPanel';
+import { RemuneratedCashTrajectoryPanel } from './RemuneratedCashTrajectoryPanel';
 import { StrategyConsensusPanel } from './StrategyConsensusPanel';
 
 interface Props {
@@ -23,6 +24,8 @@ interface Props {
 export const MarketUtilityDashboard: React.FC<Props> = ({ scan, decision, positionHealth, onInspectAsset }) => {
   return <section className="space-y-4">
     <CurrentOpportunityAlertsPanel scan={scan} decision={decision} positionHealth={positionHealth} onInspectAsset={onInspectAsset} />
+
+    <RemuneratedCashTrajectoryPanel mode="LIVE" principalEur={decision.cashAmountEur} horizonMonths={decision.horizonYears * 12} />
 
     <RealPurchaseRegistrationPanel scan={scan} decision={decision} positionHealth={positionHealth} />
 
