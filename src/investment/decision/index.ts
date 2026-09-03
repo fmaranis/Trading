2da014f1e1737c64d3bde0b6d6db5fa8b5a333ff
fmaranis <fmaranis@gmail.com>
2026-09-04 00:35:07 +0200

    Export V1 pilot decision history

--- src/investment/decision/index.ts
+++ src/investment/decision/index.ts
@@ -1,6 +1,7 @@
 export * from './types';
 export * from './investmentDecisionEngine';
 export * from './decisionHistoryService';
+export * from './pilotDecisionHistory';
 export * from './decisionBacktestEngine';
 export * from './assetUniverse';
 export * from './assetUniverseScanner';
