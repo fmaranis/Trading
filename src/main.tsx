cdec988d4571659936ce1073a92a47cf74f9ac50
fmaranis <fmaranis@gmail.com>
2026-09-10 08:14:31 +0200

    Revert speculative JSON download compatibility hook

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { SecureAppGate } from './auth/SecureAppGate';
 import App from './App.tsx';
-import { installJsonDownloadCompatibility } from './utils/jsonDownloadCompat';
 import './index.css';
-
-installJsonDownloadCompatibility();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
