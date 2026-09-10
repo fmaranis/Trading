b6215aae0cf6153791b440a476324f1c16b262a4
fmaranis <fmaranis@gmail.com>
2026-09-10 07:12:36 +0200

    Install scoped JSON download compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { SecureAppGate } from './auth/SecureAppGate';
 import App from './App.tsx';
+import { installJsonDownloadCompatibility } from './utils/jsonDownloadCompat';
 import './index.css';
+
+installJsonDownloadCompatibility();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
